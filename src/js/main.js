4150edfe02f1c0e45056bcadd20044821b306044
Jakub Goc <jakub.goc98@gmail.com>
2020-04-24 11:55:17 +0200

    .

--- src/js/main.js
+++ src/js/main.js
@@ -17,12 +17,12 @@ fetch('https://api.github.com/users/jakubgoc/repos?sort=updated&direction=desc')
           <img class="project__logo" src="assets/img/github.svg" alt="">
           <h3 class="project__title">${name}</h3>
           ${
-            description ? `<p class="project__description">${description}</p>` : 'No description'
+            description ? `<p class="project__description">${description}</p>` : `No description`
           }
         </div>
         <div class="project__footer">
           ${
-            homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow norefferer" title="Demo: ${name}">Demo</a>` : ''
+            homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow norefferer" title="Demo: ${name}">Demo</a>` : ``
           }
           <a class="project__link project__link--code" href=""${html_url}" target="_blank" rel="nofollow norefferer" title="Source code: ${name}">GitHub</a>
         </div>
