413898c2f4407042b67051e2c4a5ef0b48a92d90
Jakub Goc <jakub.goc98@gmail.com>
2020-04-24 12:12:31 +0200

    change in semantic of html

--- src/js/main.js
+++ src/js/main.js
@@ -11,23 +11,21 @@ fetch('https://api.github.com/users/jakubgoc/repos?sort=updated&direction=desc')
     for (const repo of repos) {
       const { description, homepage, html_url, name } = repo;
       list.innerHTML += `
-    
         <li class="project">
         <div class="project__container">
           <img class="project__logo" src="assets/img/github.svg" alt="">
           <h3 class="project__title">${name}</h3>
           ${
-            description ? `<p class="project__description">${description}</p>` : `No description`
+            description ? `<p class="project__description">${description}</p>` : 'No description'
           }
         </div>
         <div class="project__footer">
-          ${
-            homepage ? `<a class="project__link project__link--demo" href="${homepage}" target="_blank" rel="nofollow norefferer" title="Demo: ${name}">Demo</a>` : ``
-          }
-          <a class="project__link project__link--code" href=""${html_url}" target="_blank" rel="nofollow norefferer" title="Source code: ${name}">GitHub</a>
+        ${
+          homepage ? `<a class="project__link" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}.">Demo</a>` : ''
+        }
+        <a class="project__link project__link--code" href="${html_url}" target="_blank" rel="nofollow noreferrer"  title="Source code: ${name}.">GitHub</a>
         </div>
       </li>
-    
     `;
     }
   })
